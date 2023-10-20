9273d4cc57028a5a71c958bd241aee29ec0f9562
Jennifer Peterson <jennpeterson986@gmail.com>
2023-10-19 21:57:23 -0700

    Documented Book class.

--- ts/main.ts
+++ ts/main.ts
@@ -2,9 +2,26 @@
 *Represents an individual book that can be purchased
 */
 class Book {
+
+    /**
+     *Book's 13-digit ISBN
+     */
     isbn!: string;
+    
+    /**
+     * Title of the book
+     */
     title! : string;
+    
+    /**
+     *Retail price of the book 
+     */
     price! : number;
+    
+    /**
+     * The date the book was First published.
+     * Date can also be an announced future release date.
+     */
     releaseDate : Date | undefined;
 
 }
